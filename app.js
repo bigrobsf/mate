9adb3daba5fd01683903e4f9c41650605cdc1be8
bigrobsf <rjconner@mac.com>
2017-02-14 22:35:50 -0800

    update package.json, force redirect to https

--- app.js
+++ app.js
@@ -25,6 +25,20 @@ const index = require('./routes/index.js');
 const photos = require('./routes/photos.js');
 const chat = require('./routes/chat.js');
 
+var https_redirect = (req, res, next) => {
+    if (process.env.NODE_ENV === 'production') {
+        if (req.headers['x-forwarded-proto'] != 'https') {
+            return res.redirect('https://' + req.headers.host + req.url);
+        } else {
+            return next();
+        }
+    } else {
+        return next();
+    }
+};
+
+app.use(https_redirect);
+
 app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
 app.use(methodOverride('_method'));
 app.use(cookieParser());
